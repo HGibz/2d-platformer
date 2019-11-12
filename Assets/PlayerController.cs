6a7aa4f4dffd71abd5bf02af68888d3e795bec07
HGibz <HGibz@users.noreply.github.com>
2019-11-12 14:36:35 +0000

    clean up on code and player running movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+        
+           
+       
+
     private void Update()
     {
-      if(Input .GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+        }
+        else 
+        {
+            anim.SetBool("running", false);
+
         }
 
         if(Input.GetKeyDown(KeyCode.W))
         {
             rb.velocity = new Vector2(rb.velocity.x, 5f);
         }
+
     }
 }
 
