d1c2ca433a2a900f047f6bfaac0335d5d1abd049
HGibz <HGibz@users.noreply.github.com>
2019-11-18 13:52:05 +0000

    jump only when on ground, jumping and falling animation.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,13 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    private enum State {idle, running, jumping}
+    private enum State {idle, running, jumping, falling}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
 
     private void Start()
-    {
+
+  
+     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-    }
+        coll = GetComponent<Collider2D>();
+     }
         
            
        
@@ -23,25 +29,57 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
         }
         else 
         {
-            anim.SetBool("running", false);
+            
 
         }
 
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, 5f);
+            state = State.jumping;
         }
 
+        VelocityState();
+        anim.SetInteger("state", (int)state);
     }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
+        {
+            //moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+      
+    }
+
+
+
 }
 
