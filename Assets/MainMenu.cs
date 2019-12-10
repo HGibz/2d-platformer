bd4cfd8dba11b6615c78368eee42b93eca58093d
HGibz <HGibz@users.noreply.github.com>
2019-12-10 16:25:18 +0000

    new art design, main menu.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button newStartGameButton;
+
+    public Button newControlsButton;
+
+
+    public Button newInfoButton;
+
+    public string newGameSceneName;
+
+    public string newControlSceneName;
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+    public void ControlsButton()
+    {
+        SceneManager.LoadScene(newControlSceneName);
+    }
+
+
+    public void InfoButton()
+    {
+
+    }
+}
